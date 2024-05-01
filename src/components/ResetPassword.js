19adf67a304b2b10fadff43d12b6715af9933f18
thaophuong1609 <phuongnguyen.31211024695@st.ueh.edu.vn>
2024-05-01 21:52:41 +0700

    first commit

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 function ResetPassword(props) {
-    return (props.trigger) ? (
+    return  (
         <div className="modal-container">
             <div className="popup-inner">
 
-                {props.children}
+               
                 <button className="bluecolor block m-2 bg-0096FF hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" onClick={() => props.setTrigger(false)}>
                     Xác nhận
                 </button>
             </div>
         </div>
-    ) : ""
+    ) 
 }
 export default ResetPassword;
